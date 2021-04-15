7f71e13cd9090300928a020e79a190be1ba567db
Code4Blessings <robincw35@gmail.com>
2021-04-14 22:37:24 -0400

    Added all contents back to welcome page and created one widget

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -16,8 +16,80 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         child: Container(
           height: MediaQuery.of(context).size.height,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Image.asset('assets/images/crowded.jpg')
+              ClipPath(
+                //clipper: CurveClipper(),
+                child: Image.asset(
+                  'assets/images/crowded.jpg',
+                  height: MediaQuery.of(context).size.height / 2.5,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(height: 15),
+              Text(
+                'Welcome to the Capacity Counter',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.0,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 10.0),
+              Padding(
+                padding: const EdgeInsets.all(17.0),
+                child: Text(
+                  'The CDC recommends businesses operate at a reduced capacity during the pandemic to minimize the risk of transmitting Covid-19. Please exercise caution and wait to enter if you are asked.',
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      letterSpacing: 2.0,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  'By working together we can keep our communities healthy, happy, and safe!',
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      letterSpacing: 2.0,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                    'Thank you!',
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      letterSpacing: 2.0,
+                      fontWeight: FontWeight.bold
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: RaisedButton(
+                  child: Text(
+                      'CONTINUE',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () => Navigator.pushNamed(context, '/home'),
+                  color: Theme.of(context).primaryColor,
+                ),
+              )
             ],
           ),
         ),
---
+++ lib/widgets/curve_clipper.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CurveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    throw UnimplementedError();
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    throw UnimplementedError();
+  }
+
+}
